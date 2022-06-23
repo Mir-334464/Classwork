7095b61ff6913e9dde3bef82e19bb833aa8c8764
Mir-334464 <106953464+Mir-334464@users.noreply.github.com>
2022-06-23 13:48:41 +0530

    Add files via upload

---
+++ Vowel_Switch.java
@@ -0,0 +1,96 @@
+
+import java.util.*;
+
+class Vowel_Switch
+
+  {
+
+    public static void main(String[]args)
+
+     {
+
+         Scanner sc=new Scanner(System.in);
+
+         System.out.print("\n\t\t\tEnter Any Alphabet: ");
+
+         char CH=sc.next().charAt(0);
+
+switch(CH)
+  {
+
+    case 'a':
+    
+     System.out.println("\n\t\t\tEntered Alphabet is Vowel");
+     break; 
+    
+
+case 'e':
+
+System.out.println("\n\t\t\tEntered Alphabet is Vowel");
+break;
+
+
+case 'i':
+
+System.out.println("\n\t\t\tEntered Alphabet is Vowel");
+break;
+
+
+case 'o':
+
+System.out.println("\n\t\t\tEntered Alphabet is Vowel");
+break;
+
+
+case 'u':
+
+System.out.println("\n\t\t\tEntered Alphabet is Vowel");
+break;
+
+
+case 'A':
+
+System.out.println("\n\t\t\tEntered Alphabet is Vowel");
+break;
+
+
+case 'E':
+
+System.out.println("\n\t\t\tEntered Alphabet is Vowel");
+break;
+
+
+case 'I':
+
+System.out.println("\n\t\t\tEntered Alphabet is Vowel");
+break;
+
+
+case 'O':
+
+System.out.println("\n\t\t\tEntered Alphabet is Vowel");
+break;
+
+
+case 'U':
+
+System.out.println("\n\t\t\tEntered Alphabet is Vowel");
+break;
+
+
+default:
+
+System.out.println("\n\t\t\tEntered Alphabet is Consonant");
+
+
+          }
+
+     }
+
+}
+
+
+
+
+
+
