8956e13ea3c32a68e8392ab84f2dfc92a7ae7e84
Mir-334464 <106953464+Mir-334464@users.noreply.github.com>
2022-06-24 13:41:40 +0530

    Update Loop_Interest.java

--- Loop_Interest.java
+++ Loop_Interest.java
@@ -2,21 +2,21 @@
        Year :1 
                      
                      Amount=
-                    Rate=
-                   Interest:
-                 Total Payable Amount=( Amunt+ Interest)
+                     Rate=
+                     Interest:
+                     Total Payable Amount=( Amount+ Interest)
          Year : 2
                      
                      Amount=
-                    Rate=
-                   Interest:
-                 Total Payable Amount=( Amunt+ Interest)
+                     Rate=
+                     Interest:
+                     Total Payable Amount=( Amount+ Interest)
             Year :3 
                      
-                     Amount=
+                    Amount=
                     Rate=
-                   Interest:
-                 Total Payable Amount=( Amunt+ Interest) */
+                    Interest:
+                    Total Payable Amount=( Amount+ Interest) */
 
 import java.util.*;
 
@@ -28,27 +28,25 @@ class Loop_Interest
 
 
 System.out.print("\n\t\t\tEnter Principle Amount : ");
-			double p = sc.nextDouble();
+			double P = sc.nextDouble();
 		System.out.print("\t\t\tEnter Rate of Interest : ");
-			double r = sc.nextDouble();
-			double t = 3.0d;
+			double R = sc.nextDouble();
+			double T = 3.0d;
 		System.out.println("\t\t\t===========================================");
 		 int i = 1;
-		System.out.println("\t\t\t===========================================");
-
+		
 		while(i<=3)
 		{
 			if(i==1)
 			{
 				System.out.println("\t\t\tYear 1 :");
-				double SI = (p*r*1)/100;
-				System.out.println("\t\t\tPrinciple Amount is = " +p);
-				System.out.println("\t\t\tRate of Interest is = " +r);
-				System.out.println("\t\t\tTime Duration is = " +t);
+				double SI = (P*R*1)/100;
+				System.out.println("\t\t\tPrinciple Amount is = " +P);
+				System.out.println("\t\t\tRate of Interest is = " +R);
+				System.out.println("\t\t\tTime Duration is = " +T);
 				System.out.println("\t\t\tInterest Amount is = " +SI);
-				double pa = p+SI;
-				System.out.println("\t\t\t Total payable amount = " +pa);
-				System.out.println("\t\t\t===========================================");
+				double PA = P+SI;
+				System.out.println("\t\t\t Total payable amount = " +PA);
 				System.out.println("\t\t\t===========================================");
 				
 			}
@@ -56,14 +54,13 @@ System.out.print("\n\t\t\tEnter Principle Amount : ");
 			else if(i==2)
 			{
 				System.out.println("\t\t\tYear 2 :");
-				double SI = (p*r*2)/100;
-				System.out.println("\t\t\tPrinciple Amount is = " +p);
-				System.out.println("\t\t\tRate of Interest is = " +r);
-				System.out.println("\t\t\tTime Duration is = " +t);
+				double SI = (P*R*2)/100;
+				System.out.println("\t\t\tPrinciple Amount is = " +P);
+				System.out.println("\t\t\tRate of Interest is = " +R);
+				System.out.println("\t\t\tTime Duration is = " +T);
 				System.out.println("\t\t\tInterest Amount is = " +SI);
-				double pa = p+SI;
-				System.out.println("\t\t\t Total payable amount = " +pa);
-				System.out.println("\t\t\t===========================================");
+				double PA = P+SI;
+				System.out.println("\t\t\t Total payable amount = " +PA);
 				System.out.println("\t\t\t===========================================");
 
 
@@ -72,12 +69,12 @@ System.out.print("\n\t\t\tEnter Principle Amount : ");
 			else if(i==3 )
 			{
 				System.out.println("\t\t\tYear 3 :");
-				double SI = (p*r*3)/100;
-				System.out.println("\t\t\tPrinciple Amount is = " +p);
-				System.out.println("\t\t\tRate of Interest is = " +r);
-				System.out.println("\t\t\tTime Duration is = " +t);
+				double SI = (P*R*3)/100;
+				System.out.println("\t\t\tPrinciple Amount is = " +P);
+				System.out.println("\t\t\tRate of Interest is = " +R);
+				System.out.println("\t\t\tTime Duration is = " +T);
 				System.out.println("\t\t\tInterest Amount is = " +SI);
-				double pa = p+SI;
+				double PA = P+SI;
 				System.out.println("\t\t\t Total payable amount = " +pa);
 				System.out.println("\t\t\t===========================================");
 				System.out.println("\t\t\t===========================================");
